1ec008c281f72087f202875749fa17e6d99f1aee
Christoph Griehl <crismon@christophs-mini.local>
2019-03-08 12:27:00 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Christoph Griehl on 08.03.19.
+//  Copyright © 2019 apparillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
